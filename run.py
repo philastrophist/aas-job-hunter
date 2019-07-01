b3810d41b9ce130a1b40f36cd6a108abb0fbd5b7
Shaun Read <shaun.c.read@gmail.com>
2019-07-01 16:20:04 +0100

    notify via mailgun

--- run.py
+++ run.py
@@ -19,7 +19,9 @@ table, new = scraper.scrape(position_name)
 
 from notifiers import get_notifier
 email = get_notifier('mailgun')
-email.notify(to=os.environ['EMAIL_ADDRESS'],
-             domain=os.environ['MAILGUN_DOMAIN'],
-             api_key=os.environ['MAILGUN_API_KEY'],
-             message='There {} new positions available!'.format(new))
+email.notify(**{'to': os.environ['EMAIL_ADDRESS'],
+             'domain': os.environ['MAILGUN_DOMAIN'],
+             'api_key': os.environ['MAILGUN_API_KEY'],
+             'from': 'postmaster@'+os.environ['MAILGUN_DOMAIN'],
+             'subject': '{} New Jobs available!'.format(new),
+             'message': 'There {} new positions available!'.format(new)})
