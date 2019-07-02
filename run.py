1d7000b6977a7e7df13d3c1b7d57967455882a86
Shaun Read <shaun.c.read@gmail.com>
2019-07-02 16:56:52 +0100

    reformat

--- run.py
+++ run.py
@@ -12,17 +12,21 @@ logging.basicConfig(level=logging.INFO)
 position_name = 'Post-doctoral Positions & Fellowships'
 
 table, new = scraper.scrape(position_name)
-assert len(new) >= 0, "somethings wrong here"
+if new is None:
+    exit(0)
+
+assert len(new) > 0, "something has gone badly wrong..."
+
 from notifiers import get_notifier
 email = get_notifier('mailgun')
 
-if len(new) > 0:
-    headers = new['Title'] + ' at ' + new['Institution/Organization'] + ' in ' + new['Location']
-    line = '='*20
-    bullets = headers + '\n' + line + '\n' + new['Job Announcement Text']
-    email.notify(**{'to': os.environ['EMAIL_ADDRESS'],
-                    'domain': os.environ['MAILGUN_DOMAIN'],
-                    'api_key': os.environ['MAILGUN_API_KEY'],
-                    'from': 'postmaster@'+os.environ['MAILGUN_DOMAIN'],
-                    'subject': '{} New Jobs available!'.format(len(new)),
-                    'message': '\n\n\n\n'.join(bullets)})
+headers = new['Title'] + ' at ' + new['Institution/Organization'] + ' in ' + new['Location']
+line = '='*20
+bullets = headers + '\n' + line + '\n' + new['Job Announcement Text']
+email.notify(**{'to': os.environ['EMAIL_ADDRESS'],
+                'domain': os.environ['MAILGUN_DOMAIN'],
+                'api_key': os.environ['MAILGUN_API_KEY'],
+                'from': 'postmaster@'+os.environ['MAILGUN_DOMAIN'],
+                'subject': '{} New Jobs available!'.format(len(new)),
+                'message': '\n\n\n\n'.join(map(str, bullets))})
+
