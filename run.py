cd39c9c2a45c7603e5322c3336766b1b148073f0
Shaun Read <shaun.c.read@gmail.com>
2019-07-02 16:36:37 +0100

    better formatting

--- run.py
+++ run.py
@@ -18,11 +18,11 @@ email = get_notifier('mailgun')
 
 if len(new) > 0:
     headers = new['Title'] + ' at ' + new['Institution/Organization'] + ' in ' + new['Location']
-    line = headers.apply(lambda x: len(x)*'=')
+    line = '='*20
     bullets = headers + '\n' + line + '\n' + new['Job Announcement Text']
     email.notify(**{'to': os.environ['EMAIL_ADDRESS'],
                     'domain': os.environ['MAILGUN_DOMAIN'],
                     'api_key': os.environ['MAILGUN_API_KEY'],
                     'from': 'postmaster@'+os.environ['MAILGUN_DOMAIN'],
                     'subject': '{} New Jobs available!'.format(len(new)),
-                    'message': '\n\n'.join(bullets)})
+                    'message': '\n\n\n\n'.join(bullets)})
